eaa1db08b8858e378e819bbea0ab3912aa298ad9
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-02-13 06:50:57 +0900

    BootStrapの追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,14 @@
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import type {AppProps} from "next/app";
 import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
+function MyApp({Component, pageProps}: AppProps) {
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
 }
 
 export default MyApp;
