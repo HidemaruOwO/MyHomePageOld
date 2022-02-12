428b94373fc5e84a8973ca94e456eda271b98c05
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-02-13 08:29:18 +0900

    フッターにサービス項目を追加&背景グレー

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,23 +1,24 @@
 import styles from "../styles/Footer.module.css";
 
 const Footer = () => {
-  return (
-    <footer>
-      <div className={styles.footer}>
-        <ul className={styles.footer_list}>
-          <li>ホーム</li>
-          <li>アプリ</li>
-          <li>ブログ</li>
-          <li>プライバシーポリシー</li>
-          <li>投稿者プロフィール</li>
-          <li>お問い合わせ</li>
-        </ul>
-        <div className={styles.copy_light}>
-          <small>© 2022 HIDEMAL inc.</small>
-        </div>
-      </div>
-    </footer>
-  );
+    return (
+        <footer>
+            <div className={styles.footer}>
+                <ul className={styles.footer_list}>
+                    <li>ホーム</li>
+                    <li>アプリ</li>
+                    <li>ブログ</li>
+                    <li>サービス</li>
+                    <li>プライバシーポリシー</li>
+                    <li>投稿者プロフィール</li>
+                    <li>お問い合わせ</li>
+                </ul>
+                <div className={styles.copy_light}>
+                    <small>© 2022 HIDEMAL inc.</small>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
