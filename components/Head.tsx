9ca52135b0639b6d39314a64369c0fbcd34d77e7
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-02-13 00:43:15 +0900

    BootStrapをHeadから削除

--- components/Head.tsx
+++ components/Head.tsx
@@ -2,71 +2,61 @@ import Head from "next/head";
 import Layout from "./Layout";
 
 type HeadI = {
-  title: string;
-  description: string;
-  dir: string;
+    title: string;
+    description: string;
+    dir: string;
+    image: string;
 };
 
-const HeadInfo = ({ title, description, dir }: HeadI) => {
-  const domain: string = "hide0.net";
-  const favicon: string = "/favicon.ico"; //size is 64x64
-  const faviconPng: string = "/favicon_128x128.png"; //size is 128x128
-  const ogpImage: string = "/ogp.jpg"; //size is 1200x630
-  const author: string = "hidemal";
-  var dir: string = dir;
-  var title: string = title;
-  var pageDescription: string = description;
-  var pageTitle: string = `${title} - Hide0`;
-  var pageUrl: string = "https://" + domain + "/" + dir;
-  var pageType: string;
-  if (pageUrl == "https://hide0.net/") {
-    pageType = "website";
-  } else {
-    pageType = "article";
-  }
-  return (
-    <Head>
-      <title>{pageTitle}</title>
-      <link rel="canonical" href={pageUrl} />
-      <link
-        href="/css/bootstrap.min.css"
-        rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-        crossOrigin="anonymous"
-      />
-      <script
-        src="/js/bootstrap.bundle.min.js"
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-        crossOrigin="anonymous"
-      />
-      <meta name="author" content={author} />
-      <meta name="description" content={pageDescription} />
-      <meta charSet="utf-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0"
-      />
-      <link rel="icon" href={favicon} />
-      <link rel="shortcut icon" href={favicon} />
-      <link rel="apple-touch-icon-precomposed" href={faviconPng} />
-      <meta name="msapplication-TileImage" content={faviconPng} />
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      {/* OGP */}
-      <meta property="og:url" content={pageUrl} />
-      <meta property="og:title" content={title} />
-      <meta property="og:type" content={pageType} />
-      <meta property="og:description" content={pageDescription} />
-      <meta property="og:image" content={ogpImage} />
-      <meta property="og:site_name" content="Hide0.Net" />
-      <meta property="og:local" content="ja_JP" />
-      {/* twitter */}
-      <meta name="twitter:card" content="Summary Card" />
-      <meta name="twitter:site" content="@Hidemal_OwO" />
-      <meta property="twitter:title" content={title} />
-      <meta property="twitter:description" content={pageDescription} />
-      <meta property="twitter:image" content={ogpImage} />
-    </Head>
-  );
+const HeadInfo = ({title, description, dir, image}: HeadI) => {
+    const domain: string = "hide0.net";
+    const favicon: string = "/favicon.ico"; //size is 64x64
+    const faviconPng: string = "/favicon_128x128.png"; //size is 128x128
+    const ogpImage: string = image; //size is 1200x630
+    const author: string = "hidemal";
+    var dir: string = dir;
+    var title: string = title;
+    var pageDescription: string = description;
+    var pageTitle: string = `${title} - Hide0`;
+    var pageUrl: string = "https://" + domain + "/" + dir;
+    var pageType: string;
+    if (pageUrl == "https://hide0.net/") {
+        pageType = "website";
+    } else {
+        pageType = "article";
+    }
+    return (
+        <Head>
+            <title>{pageTitle}</title>
+            <link rel="canonical" href={pageUrl}/>
+            <meta name="author" content={author}/>
+            <meta name="description" content={pageDescription}/>
+            <meta charSet="utf-8"/>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1.0, user-scalable=1.0, minimum-scale=1.0, maximum-scale=1.0"
+            />
+            <link rel="icon" href={favicon}/>
+            <link rel="shortcut icon" href={favicon}/>
+            <link rel="apple-touch-icon-precomposed" href={faviconPng}/>
+            <meta name="msapplication-TileImage" content={faviconPng}/>
+            <meta name="msapplication-TileColor" content="#ffffff"/>
+            {/* OGP */}
+            <meta property="og:url" content={pageUrl}/>
+            <meta property="og:title" content={title}/>
+            <meta property="og:type" content={pageType}/>
+            <meta property="og:description" content={pageDescription}/>
+            <meta property="og:image" content={ogpImage}/>
+            <meta property="og:site_name" content="Hide0.Net"/>
+            <meta property="og:local" content="ja_JP"/>
+            {/* twitter */}
+            <meta name="twitter:card" content="Summary Card"/>
+            <meta name="twitter:site" content="@Hidemal_OwO"/>
+            <meta property="twitter:title" content={title}/>
+            <meta property="twitter:description" content={pageDescription}/>
+            <meta property="twitter:image" content={ogpImage}/>
+        </Head>
+    );
 };
 
 export default HeadInfo;
