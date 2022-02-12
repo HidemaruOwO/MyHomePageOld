bb236a7afbd3afa2e4045b2bc9b4653b1a974fc9
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-02-13 06:50:32 +0900

    Headerの修正

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,26 @@
 import styles from "../styles/Header.module.css";
 
 const Header = () => {
-  return (
-    <header>
-      <div className={styles.header}>
-        <ul>
-          <li className={styles.logo}>
-            <img src="/monochrome_slime.svg" height="100%" width="100%" />
-          </li>
-          <li className={styles.navigation}>
-            <a href="https://blog.hide0.net">Blog</a>
-          </li>
-          <li className={styles.navigation}>
-            <a href="/app">App</a>
-          </li>
-          <li className={styles.navigation}>
-            <a href="/">HOME</a>
-          </li>
-        </ul>
-      </div>
-    </header>
-  );
+    return (
+        <header>
+            <div className={styles.header}>
+                <ul>
+                    <li className={styles.logo}>
+                        <img src="/monochrome_slime.svg" height="100%" width="100%"/>
+                    </li>
+                    <li className={styles.navigation}>
+                        <a href="https://blog.hide0.net">Blog</a>
+                    </li>
+                    <li className={styles.navigation}>
+                        <a href="/app">App</a>
+                    </li>
+                    <li className={styles.navigation}>
+                        <a href="/">HOME</a>
+                    </li>
+                </ul>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,19 +1,19 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import { ReactNode } from "react";
+import {ReactNode} from "react";
 
 interface Props {
-  children: ReactNode;
+    children: ReactNode;
 }
 
-const Layout = ({ children }: Props) => {
-  return (
-    <div>
-      <Header />
-      <main>{children}</main>
-      <Footer />
-    </div>
-  );
+const Layout = ({children}: Props) => {
+    return (
+        <div>
+            <Header/>
+            <main>{children}</main>
+            <Footer/>
+        </div>
+    );
 };
 
 export default Layout;
