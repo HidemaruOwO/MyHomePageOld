8b201748441f7902cc81707f06704d1b265fa575
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-02-13 10:10:50 +0900

    appListのデータを追加

---
+++ data/appList.ts
@@ -0,0 +1,6 @@
+const appList = [
+    {title:"未作成", description:"None", image:"/image/none.jpeg", url:"/app/none"},
+    {title:"未作成", description:"None", image:"/image/none.jpeg", url:"/app/none"},
+    {title:"未作成", description:"None", image:"/image/none.jpeg", url:"/app/none"}
+];
+export default appList;
