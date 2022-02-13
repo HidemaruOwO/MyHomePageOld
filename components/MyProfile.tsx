a2ba2d5b57a213d20a66ffca175052c34ec8efa3
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-02-14 06:21:12 +0900

    全体が中央になるように修正

--- components/MyProfile.tsx
+++ components/MyProfile.tsx
@@ -1,10 +1,11 @@
 import {Card} from "react-bootstrap";
 import styles from "../styles/Sns.module.css";
+import styles2 from "../styles/MyProfile.module.css";
 
 const MyProfile = () => {
     return (
         <>
-            <Card style={{width: '18rem'}}>
+            <Card style={{width: '20rem'}} className={styles2.center}>
                 <Card.Img variant="top" src="/image/slime_squre_icon.png"/>
                 <Card.Body>
                     <Card.Title>ひでまる</Card.Title>
