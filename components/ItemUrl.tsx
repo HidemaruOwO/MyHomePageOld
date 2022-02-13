85da5120de10144a86bfe7e878e2576af03c2f59
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-02-13 10:10:39 +0900

    Item urlを追加

---
+++ components/ItemUrl.tsx
@@ -0,0 +1,29 @@
+import {Card} from "react-bootstrap";
+import styles from "../styles/ItemUrl.module.css";
+
+type ItemProps = {
+    title: string;
+    description: string;
+    image: string;
+    url: string;
+}
+const ItemUrl = ({title, description, image, url}: ItemProps) => {
+    return (
+        <>
+
+            <Card style={{width: '18rem'}} className={styles.noline}>
+                <a href={url}>
+                    <Card.Img variant="top" src={image} />
+                    <Card.Body>
+                        <Card.Title>{title}</Card.Title>
+                        <Card.Text>
+                            {description}
+                        </Card.Text>
+                    </Card.Body>
+                </a>
+            </Card>
+        </>
+    );
+}
+
+export default ItemUrl;
