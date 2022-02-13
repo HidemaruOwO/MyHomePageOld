de9056ecc297d74d2fe5213b8cbe1c5c694d3560
HidemaruOwO <82384920+HidemaruOwO@users.noreply.github.com>
2022-02-13 11:12:24 +0900

    "New App"項目の追加

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type {NextPage} from "next";
 import Image from "next/image";
 import {useState} from "react";
-import {Container, Row} from "react-bootstrap";
+import {Container, Row, Button} from "react-bootstrap";
 import styles from "../styles/Home.module.css";
 import HeadInfo from "../components/Head";
+import ItemUrl from "../components/ItemUrl";
+import appList from "../data/appList";
 
 type TopPage = {
     message: string;
@@ -38,17 +40,17 @@ const Home: NextPage = () => {
             page: "/app"
         }
     ];
-    var topPageItemsNumber:number = 0;
+    var topPageItemsNumber: number = 0;
     const [topPage, setTopPage] = useState<TopPage>(topPageItems[0]);
     const redoTopPageItem = () => {
         topPageItemsNumber++;
-        if (topPageItemsNumber==topPageItems.length) {
+        if (topPageItemsNumber == topPageItems.length) {
             topPageItemsNumber = 0;
         }
         setTopPage(topPageItems[topPageItemsNumber]);
-        console.log(topPageItemsNumber+"|"+topPageItems.length);
+        console.log(topPageItemsNumber + "|" + topPageItems.length);
     }
-    setInterval(redoTopPageItem,4000);
+    //setInterval(redoTopPageItem,4000);
     return (
         <>
             <HeadInfo title="ホーム" description="一学生がプログラミングをして提供する有用な記事と便利なソフトを集めたひでまるの公式サイト" dir="/"
@@ -68,11 +70,22 @@ const Home: NextPage = () => {
                 </a>
             </div>
             <Container className={styles.container}>
-                <Row id={"aboutSite"}>
+                <Row id={"aboutSite"} className={styles.row}>
                     <h2>このサイトについて</h2>
                     <p>このサイトは一人の学生が便利なソフトや有益な情報を多くの人に知ってもらいたいという思いを込めて作られたサイトです。<br/>
                         日が経つ度にサイトは更新されるので、定期的に確認してもらうと嬉しいです。</p>
                     <img src={"/image/ogp/index.png"} alt={"このサイトの紹介"}/>
+                </Row>
+                <Row className={styles.row}>
+                    <h2>New App</h2>
+                    <p>「あったらいいな」を実現するウェブアプリをご利用ください!</p>
+                    <ItemUrl title={appList[0].title} description={appList[0].description} image={appList[0].image}
+                             url={appList[0].url}/>
+                    <ItemUrl title={appList[1].title} description={appList[1].description} image={appList[1].image}
+                             url={appList[1].url}/>
+                    <ItemUrl title={appList[2].title} description={appList[2].description} image={appList[2].image}
+                             url={appList[2].url}/>
+                    <Button variant="primary" href={"/app"}>More App</Button>
                 </Row>
             </Container>
         </>
